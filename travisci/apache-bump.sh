4d4fab9987bcaba409e8e893570af1a6954bc9c1
Matthew Laird <lairdm@ebi.ac.uk>
2016-08-26 16:19:27 +0100

    sigh

--- travisci/apache-bump.sh
+++ travisci/apache-bump.sh
@@ -13,8 +13,10 @@ curl "http://localhost/faidx?set=human&location=Y%3A1000-2000"
 curl "http://localhost/faidx/sets"
 curl "http://localhost/faidx/locations/cat/"
 curl "http://localhost/faidx/locations/human/"
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
 
 sudo pkill --signal HUP apache2
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
 (sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
 curl "http://localhost/faidx?set=human&location=1%3A1000-2000"
 curl "http://localhost/faidx?set=human&location=Y%3A1000-2000"
@@ -24,3 +26,4 @@ curl "http://localhost/faidx/locations/human/"
 
 sudo pkill --signal HUP apache2
 sleep 5
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
