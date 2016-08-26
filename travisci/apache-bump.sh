b85244ec1512fa1b27901043bcada3775f9c6563
Matthew Laird <lairdm@ebi.ac.uk>
2016-08-26 16:32:40 +0100

    try again

--- travisci/apache-bump.sh
+++ travisci/apache-bump.sh
@@ -1,12 +1,16 @@
 # We need to run apache, then restart it to get coverage to
 # work correctly
 
+lcov --directory . --zerocounters
+touch ${APACHE_FAIDX_DIR}/.libs/mod_faidx.gcda && chmod 666 ${APACHE_FAIDX_DIR}/.libs/mod_faidx.gcda && sudo chown root.root ${APACHE_FAIDX_DIR}/.libs/mod_faidx.gcda
+ls -l ${APACHE_FAIDX_DIR}/
 (sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
 
 ps auxww|grep [a]pache2
 curl "http://localhost/faidx/locations/human/"
 
-sudo pkill --signal HUP apache2
+sudo pkill -n --signal HUP apache2
+sleep 1
 (sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
 curl "http://localhost/faidx?set=human&location=1%3A1000-2000"
 curl "http://localhost/faidx?set=human&location=Y%3A1000-2000"
@@ -15,7 +19,8 @@ curl "http://localhost/faidx/locations/cat/"
 curl "http://localhost/faidx/locations/human/"
 lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
 
-sudo pkill --signal HUP apache2
+sudo pkill -n --signal HUP apache2
+sleep 1
 lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
 (sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
 curl "http://localhost/faidx?set=human&location=1%3A1000-2000"
@@ -24,6 +29,6 @@ curl "http://localhost/faidx/sets"
 curl "http://localhost/faidx/locations/cat/"
 curl "http://localhost/faidx/locations/human/"
 
-sudo pkill --signal HUP apache2
+sudo pkill -n --signal HUP apache2
 sleep 5
 lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
