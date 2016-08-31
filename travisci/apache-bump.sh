ad208ed1570615b86091418b23d53f60c6afff81
Matthew Laird <lairdm@ebi.ac.uk>
2016-08-31 10:14:30 +0100

    Damn coveralls

--- travisci/apache-bump.sh
+++ travisci/apache-bump.sh
@@ -44,3 +44,43 @@ sleep 2
 ls -l ${APACHE_FAIDX_DIR}/.libs/
 
 exit
+
+(sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
+sleep 1
+
+echo "Apache started"
+ps auxww|grep [a]pache2
+curl "http://localhost/faidx/locations/human/"
+
+echo "Killing apache"
+sudo pkill -n --signal HUP apache2
+sleep 1
+ls -l ${APACHE_FAIDX_DIR}/.libs/
+
+echo "Starting apache"
+(sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
+curl "http://localhost/faidx?set=human&location=1%3A1000-2000"
+curl "http://localhost/faidx?set=human&location=Y%3A1000-2000"
+curl "http://localhost/faidx/sets"
+curl "http://localhost/faidx/locations/cat/"
+curl "http://localhost/faidx/locations/human/"
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
+
+echo "Killing apache"
+sudo pkill -n --signal HUP apache2
+sleep 1
+
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
+
+echo "Starting Apache"
+(sudo sh -c '. /etc/apache2/envvars; /usr/sbin/apache2 -X')&
+curl "http://localhost/faidx?set=human&location=1%3A1000-2000"
+curl "http://localhost/faidx?set=human&location=Y%3A1000-2000"
+curl "http://localhost/faidx/sets"
+curl "http://localhost/faidx/locations/cat/"
+curl "http://localhost/faidx/locations/human/"
+
+echo "Killing apache"
+sudo pkill -n --signal HUP apache2
+sleep 5
+lcov --directory . --capture --output-file coverage.info && lcov --list coverage.info
