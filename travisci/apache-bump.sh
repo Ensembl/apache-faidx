9ceed6e6bc26c0088d8398335ae0f010bddf24cd
Matthew Laird <lairdm@ebi.ac.uk>
2016-08-31 10:24:46 +0100

    Damn coveralls

--- travisci/apache-bump.sh
+++ travisci/apache-bump.sh
@@ -28,12 +28,24 @@ curl "http://localhost:8000/faidx/sets"
 curl "http://localhost:8000/faidx/locations/cat/"
 curl "http://localhost:8000/faidx/locations/human/"
 
-#apachectl -k graceful
+apachectl -k graceful
 sudo sync
 sleep 2
 ls -l ${APACHE_FAIDX_DIR}/.libs/
 
 # Batch two of calls
+curl "http://localhost:8000/faidx?set=human&location=1%3A1000-2000"
+curl -H "Content-type: text/x-fasta" "http://localhost:8000/faidx?set=human&location=Y%3A1000-2000"
+curl "http://localhost:8000/faidx/sets"
+curl "http://localhost:8000/faidx/locations/cat/"
+curl "http://localhost:8000/faidx/locations/human/"
+
+apachectl -k graceful
+sudo sync
+sleep 2
+ls -l ${APACHE_FAIDX_DIR}/.libs/
+
+# Batch three of calls
 curl "http://localhost:8000/faidx?set=human&location=1%3A1000-2000"
 curl -H "Content-type: text/x-fasta" "http://localhost:8000/faidx?set=human&location=Y%3A1000-2000"
 curl "http://localhost:8000/faidx/sets"
